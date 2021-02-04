7f4d4f015b29ec5832cb69a1e695e4785dccf5ee
RapidIOT <vrksvrks20@gmail.com>
2021-02-04 16:18:52 +0530

    login and Registror UI3

---
+++ resources/views/-welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    {{-- @parent --}}
+    <p>This is appended to the master sidebar.</p>
+@stop
+
+@section('content')
+    <p>This is my body content.</p>
+@stop
