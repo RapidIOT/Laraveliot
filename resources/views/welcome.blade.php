6705cfbb2729ee8953399f48b5b8332e50f3d830
RapidIOT <vrksvrks20@gmail.com>
2021-02-06 19:19:12 +0530

    text change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Rapid IOT
+                    Rapid IOT Devices
                 </div>
 
                 <div class="links">
