69bb32abf50d2f1eaa8a94110b0c4a2bfa37ef76
RapidIOT <vrksvrks20@gmail.com>
2021-02-06 20:17:53 +0530

    remove space

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,6 @@
                 <div class="title m-b-md">
                     Rapid IOT Devices
                 </div>
-
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
